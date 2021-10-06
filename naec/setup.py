e29f9be40e9251944eccb6d51bfb06e9053270b9
michalber <michalberdzik97@gmail.com>
2021-10-06 15:53:52 +0200

    Update repo structure

---
+++ naec/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='naec',
+    version='0.0.1',
+    packages=[],
+    url='',
+    license='',
+    author='Michał Berdzik',
+    author_email='michalberdzik97@gmail.com',
+    description='Neural Network based Acoustic Echo Cancellation'
+)
